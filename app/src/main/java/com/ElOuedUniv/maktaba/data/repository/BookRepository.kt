03c33f005c6d18cbe1f39775d4ecbf8d234196bc
Ahmed  geudoura <ahmedged2005@gmail.com>
2026-03-05 14:25:37 +0100

    tp 2

---
+++ app/src/main/java/com/ElOuedUniv/maktaba/data/repository/BookRepository.kt
@@ -0,0 +1,12 @@
+package com.ElOuedUniv.maktaba.data.repository
+
+import com.ElOuedUniv.maktaba.data.model.Book
+import com.ElOuedUniv.maktaba.data.model.Category
+
+interface BookRepository {
+
+    fun getAllBooks(): List<Book>
+
+    fun getBookByIsbn(isbn: String): Book?
+
+}
